9ebb110b8564dcef31197c81becf4167bfd2e254
Sébastien Krafft <skrafft@gmail.com>
2023-05-17 11:05:05 +0200

    Fixed .well-known reachability and callback, added comment support

--- __init__.py
+++ __init__.py
@@ -22,6 +22,13 @@ lnaddress_static_files = [
     }
 ]
 
+lnaddress_redirect_paths = [
+  {
+    "from_path": "/.well-known/lnurlp",
+    "redirect_to_path": "/api/v1/well-known",
+  }
+]
+
 
 def lnaddress_renderer():
     return template_renderer(["lnbits/extensions/lnaddress/templates"])
